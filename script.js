20999483d9bc4ac3bf33ff87d80a27fac8fd6aec
Caroline Diva <00001084976626sp@al.educacao.sp.gov.br>
2024-09-12 12:15:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avance.foreach(button => {
-    button.addeventlistener('click', function(){
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getattribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classlist.remove('ativo');
-        document.getElementById(proximopasso) .classlist.add ("ativo")
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
